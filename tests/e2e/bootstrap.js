e528f2023d197d178c82b2539ebc993abfe17a1a
Icebob <mereg.norbert@gmail.com>
2018-09-23 21:40:11 +0200

    better e2e runner

--- tests/e2e/bootstrap.js
+++ tests/e2e/bootstrap.js
@@ -7,12 +7,14 @@ module.exports = broker => {
 	broker.logger.info("Starting Cypress for End-to-End testing...");
 
 	// Redirect Cypress logs to Moleculer logger
+	/*
 	const logger = broker.getLogger("CYPRESS");
 	const stream = require("stream");
 	const logStream = new stream.Stream();
 
 	logStream.writable = true;
 	logStream.write = data => logger.info(chalk.bgMagenta.bold.white("CYPRESS:"), data.toString("utf8").trim());
+	*/
 
 	// Execute Cypress
 	const execa = require("execa");
@@ -21,10 +23,14 @@ module.exports = broker => {
 		args.push("run", "--record", "--key", "920a1001-30cb-4471-8d5d-066843b6a9a3");
 	else
 		args.push("open");
-	const runner = execa(require.resolve("cypress/bin/cypress"), args);
-	runner.stdout.pipe(logStream);
+	const runner = execa(require.resolve("cypress/bin/cypress"), args, { stdin: "inherit", stdout: "inherit" });
+	//runner.stdout.pipe(logStream);
 
-	runner.on("exit", async () => await broker.stop());
+	//runner.on("exit", async () => await broker.stop());
 	runner.on("error", async () => await broker.stop());
-	runner.on("exit", code => process.exit(code));
+	runner.on("exit", async code => {
+		console.log("Cypress exited", code);
+		await broker.stop();
+		process.exit(code);
+	});
 };
