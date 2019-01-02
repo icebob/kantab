36d6f467a5d0b68ef5c428b4777751b9617e1757
Icebob <mereg.norbert@gmail.com>
2019-01-02 22:06:01 +0100

    add monitoring stuffs (prometheus, grafana)

--- backend/services/metrics.service.js
+++ backend/services/metrics.service.js
@@ -1,8 +1,29 @@
 "use strict";
 
 const Tracer = require("moleculer-console-tracer");
+const PromService = require("moleculer-prometheus");
 
-module.exports = {
-	name: "metrics",
-	mixins: [Tracer]
-};
+if (process.env.NODE_ENV == "production") {
+	// Use Prometheus in production mode
+	module.exports = {
+		name: "metrics",
+		mixins: [PromService],
+		settings: {
+			port: 3030,
+			collectDefaultMetrics: false,
+			timeout: 5 * 1000,
+		},
+
+		events: {
+		}
+	};
+
+} else {
+	// Use console tracer in development mode
+	module.exports = {
+		name: "metrics",
+		mixins: [Tracer]
+	};
+}
+
+
