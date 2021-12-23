38933b6ddd966c01149c6d05711f1187dd69a469
Icebob <mereg.norbert@gmail.com>
2021-12-23 15:27:35 +0100

    add logo

--- frontend/src/components/Logo.vue
+++ frontend/src/components/Logo.vue
@@ -1,6 +1,7 @@
 <template>
-	<h3 class="font-bold text-shadow" @click="$router.push('/')">
-		<span class="text-primary">Kan</span>
-		<span class="text-text">Tab</span>
+	<h3 class="font-bold text-shadow flex items-center" @click="$router.push('/')">
+		<img src="../assets/board-icon.svg" alt="logo" class="w-9 h-9 mr-2" />
+		<span class="text-text">Kan</span>
+		<span class="text-primary">Tab</span>
 	</h3>
 </template>
